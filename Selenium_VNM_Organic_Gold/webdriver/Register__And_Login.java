2ddfa50e1a0a2dd908ce152ed134dde1580e1414
haiyen <haiyenn@gmail.com>
2020-08-10 17:56:20 +0700

    login with empty email and password

--- Selenium_VNM_Organic_Gold/webdriver/Register__And_Login.java
+++ Selenium_VNM_Organic_Gold/webdriver/Register__And_Login.java
@@ -5,7 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-//import org.testng.Assert;
+import org.testng.Assert;
 //import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,7 +16,7 @@ public class Register__And_Login {
 	@BeforeClass
 	public void beforeClass() {
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 	}
 
@@ -25,7 +25,12 @@ public class Register__And_Login {
 		driver.get("https://www.vinamilk.com.vn/dinh-duong-organic/vi/san-pham/sua-bot-tre-em-organic-gold/");	
 		driver.findElement(By.linkText("Đăng nhập")).click();
 		
-		driver.findElement(By.className("btn-common"));
+		driver.findElement(By.xpath("//ul[@class='login-tabs__nav tabs__nav']//a[contains(text(),'Đăng nhập')]")).click();
+		driver.findElement(By.xpath("//a[@class='btn-common']//span[contains(text(),'Đăng nhập')]")).click();
+		
+		//Assert.assertEquals(driver.findElement(By.xpath("//p[@id='user_login_mess']")).getText(), "Vui lòng nhập Email");
+		String emailempt = driver.findElement(By.xpath("//p[@id='user_login_mess']")).getText();
+		Assert.assertTrue(emailempt.contains("Vui lòng nhập Email"));
 	}
 //	 @AfterClass
 //	 public void afterClass() {
