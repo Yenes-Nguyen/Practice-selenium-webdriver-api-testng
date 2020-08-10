56d9c96049f28a7a0127e719d4e7391e104a794e
haiyen <haiyenn@gmail.com>
2020-08-10 16:36:17 +0700

    add index

--- Selenium_VNM_Organic_Gold/webdriver/Register__And_Login.java
+++ Selenium_VNM_Organic_Gold/webdriver/Register__And_Login.java
@@ -1,5 +1,39 @@
 package webdriver;
 
-public class Register_Login {
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Register__And_Login {
+	WebDriver driver;
+	 
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test 
+	public void TC_01_Login_With_Empty_Email_And_Password() {
+		driver.get("http://live.demoguru99.com/");	
+		driver.findElement(By.xpath("//div[@class ='footer']//a[@title='My Account']")).click();
+		
+		driver.findElement(By.id("email")).sendKeys("");
+		driver.findElement(By.name("login[password]")).sendKeys("");
+		
+		driver.findElement(By.xpath("//button[@title ='Login']")).click();
+		 
+		String emailErrorMessage = driver.findElement(By.xpath("//div[@id='advice-required-entry-email']")).getText();
+		Assert.assertEquals(emailErrorMessage, "This is a required field.");
+		
+		String passwordErrorMessage = driver.findElement(By.xpath("//div[@id='advice-required-entry-pass']")).getText();
+		Assert.assertEquals(passwordErrorMessage, "This is a required field.");
+	}
 
 }
